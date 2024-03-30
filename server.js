0e69dd3fff10f266ce4fcdee6e7b476723086b0b
Michael Garrett <57734446+mgarrett82@users.noreply.github.com>
2024-03-30 13:36:56 +1100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const res = require("express/lib/response");
 const app = express();
 const port = 3000;
 
+//display to the console the port variable 3000
 app.listen(port,()=>{
     console.log("Hello I'm listening to port"+port);
 })
